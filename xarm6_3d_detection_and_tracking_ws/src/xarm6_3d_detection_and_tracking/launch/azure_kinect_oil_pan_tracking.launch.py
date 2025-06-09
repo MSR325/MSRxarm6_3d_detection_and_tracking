cdd1bb626291416906aa80a4e33f435c25ef8d2e
Christian <christianvillarrealt@gmail.com>
2025-06-09 14:02:10 -0600

    Fixed launch file to run oil pan tracking node and ros2 azure kinect driver node to publish rbgd and point cloud info

--- xarm6_3d_detection_and_tracking_ws/src/xarm6_3d_detection_and_tracking/launch/azure_kinect_oil_pan_tracking.launch.py
+++ xarm6_3d_detection_and_tracking_ws/src/xarm6_3d_detection_and_tracking/launch/azure_kinect_oil_pan_tracking.launch.py
@@ -3,12 +3,15 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        # Azure Kinect ROS Driver Node
         Node(
             package='azure_kinect_ros_driver',
             executable='node',
             name='azure_kinect',
             parameters=[{'rgb_camera_info': True}]
         ),
+        
+        # XArm6 3D Detection and Tracking Node
         Node(
             package='xarm6_3d_detection_and_tracking',
             executable='azure_kinect_oil_pan_tracking_node',
